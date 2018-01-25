19d800b21f94d02375950a9bc64dc28b73f3bb91
Andrei Iacob <29441887+andre2i@users.noreply.github.com>
2018-01-24 17:43:47 -0800

    Removing redis dependency, still reproes

--- chunked-reproducer/src/main/java/com/example/demo/MainVerticle.java
+++ chunked-reproducer/src/main/java/com/example/demo/MainVerticle.java
@@ -12,116 +12,62 @@ import io.vertx.core.Handler;
 import io.vertx.core.http.HttpHeaders;
 import io.vertx.core.http.HttpServerOptions;
 import io.vertx.core.http.HttpServerResponse;
-import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.net.JksOptions;
-import io.vertx.redis.RedisClient;
-import io.vertx.redis.RedisOptions;
 
 public class MainVerticle extends AbstractVerticle {
     private static final int KEY_COUNT = 10000;
-    private static final int REDIS_DB = 10;
-    private static final String KEY_PREFIX = "chunked:reproducer:";
-    private RedisClient redis;
-
-    Future<Void> fillRedis() {
-        Future<Void> future = Future.future();
-        AtomicInteger keysLeft = new AtomicInteger(KEY_COUNT);
-        System.out.println("Fillig Redis");
-        redis.select(REDIS_DB, res -> {
-            redis.flushdb(flush -> {
-                for (int i = 0; i < KEY_COUNT; i++) {
-                    redis.set(KEY_PREFIX + Integer.toString(i) + ":" + randomString(), makePayload(), result -> {
-                        if (keysLeft.decrementAndGet() == 0) {
-                            future.complete();
-                        }
-                    });
-                }
-            });
-        });
-
-        return future;
-    }
 
     void getJsons(Handler<String> jsonHandler, Handler<Void> endHandler) {
-        Future<JsonArray> keysFuture = Future.<JsonArray>future().setHandler(ar -> {
-            jsonForKeys(ar.result(), jsonHandler, endHandler);
-        });
-
-        redis.keys(KEY_PREFIX + "*", keysFuture.completer());
-    }
-
-    void jsonForKeys(JsonArray keys, Handler<String> jsonHandler, Handler<Void> endHandler) {
-        int keyCount = keys.size();
-        System.out.format("Got %d keys \n", keyCount);
-        if (keyCount == 0) {
-            endHandler.handle(null);
-            return;
-        }
-
-        AtomicInteger outstandingGetRequestCount = new AtomicInteger(keyCount);
-        keys.stream().map(Object::toString).forEach(key -> {
-            jsonForKey(key, ar -> {
+        AtomicInteger outstandingGetRequestCount = new AtomicInteger(KEY_COUNT);
+        for (int i = 0; i < KEY_COUNT; i++) {
+            json(ar -> {
                 int remainingGets = outstandingGetRequestCount.decrementAndGet();
                 if (ar.succeeded())
                     jsonHandler.handle(ar.result());
                 if (remainingGets == 0)
                     endHandler.handle(null);
             });
-        });
+        }
     }
 
-    void jsonForKey(String key, Handler<AsyncResult<String>> handler) {
-        redis.get(key, get -> {
-            if (get.succeeded()) {
-                handler.handle(Future.succeededFuture(get.result()));
-            } else {
-                handler.handle(Future.failedFuture(get.cause()));
-            }
-        });
+    void json(Handler<AsyncResult<String>> handler) {
+        handler.handle(Future.succeededFuture(makePayload()));
     }
 
     @Override
     public void start(Future<Void> startFuture) throws Exception {
-        redis = RedisClient.create(vertx, new RedisOptions());
         ClassLoader classLoader = getClass().getClassLoader();
         String jksPath = classLoader.getResource("tls/server-keystore.jks").getFile();
         HttpServerOptions serverOptions = new HttpServerOptions().setSsl(true)
                 .setKeyCertOptions(new JksOptions().setPath(jksPath).setPassword("wibble"));
 
-        Future<Void> start = fillRedis().compose(v -> {
-            Future<Void> startWebServer = Future.future();
-            vertx.createHttpServer(serverOptions).requestHandler(req -> {
-                System.out.println("Received a request");
+        vertx.createHttpServer(serverOptions).requestHandler(req -> {
+            System.out.println("Received a request");
 
-                HttpServerResponse response = req.response().setChunked(true);
-                AtomicBoolean fresh = new AtomicBoolean(true);
-                response.write("[");
+            HttpServerResponse response = req.response().setChunked(true);
+            AtomicBoolean fresh = new AtomicBoolean(true);
+            response.write("[");
 
-                Handler<String> jsonHandler = json -> {
-                    if (fresh.get())
-                        fresh.set(false);
-                    else
-                        response.write(",");
-                    response.write(json);
-                };
+            Handler<String> jsonHandler = json -> {
+                if (fresh.get())
+                    fresh.set(false);
+                else
+                    response.write(",");
+                response.write(json);
+            };
 
-                Handler<Void> endHandler = end -> {
-                    response.write("]");
-                    response.putHeader(HttpHeaders.CONTENT_TYPE, "application/json")
-                            .setStatusCode(HttpURLConnection.HTTP_OK).end();
-                };
+            Handler<Void> endHandler = end -> {
+                response.write("]");
+                response.putHeader(HttpHeaders.CONTENT_TYPE, "application/json")
+                        .setStatusCode(HttpURLConnection.HTTP_OK).end();
+            };
 
-                getJsons(jsonHandler, endHandler);
-            }).exceptionHandler(exception -> {
-                System.out.println("Server error >> " + exception);
-            }).listen(8080, listen -> {
-                startWebServer.complete();
-            });
+            getJsons(jsonHandler, endHandler);
+        }).exceptionHandler(exception -> {
+            System.out.println("Server error >> " + exception);
+        }).listen(8080);
 
-            return startWebServer;
-        });
-        start.setHandler(startFuture.completer());
         System.out.println("HTTP server started on port 8080");
     }
 
